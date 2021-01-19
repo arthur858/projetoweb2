c32403434c68779038ce84afbbbf82e967536592
arthur858 <arthursylva25@gmail.com>
2021-01-19 15:15:42 -0300

    Scripts tabelas

---
+++ database/factories/FuncionarioFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\funcionario;
+use Faker\Generator as Faker;
+
+$factory->define(funcionario::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ProdutoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\produto;
+use Faker\Generator as Faker;
+
+$factory->define(produto::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
