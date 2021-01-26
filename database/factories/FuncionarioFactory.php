878be0950cfc0c701bc24952ce398a7e58a0ebf9
arthur858 <arthursylva25@gmail.com>
2021-01-25 21:30:26 -0300

    alterando o factory da tabela funcionario

--- database/factories/FuncionarioFactory.php
+++ database/factories/FuncionarioFactory.php
@@ -7,6 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(funcionario::class, function (Faker $faker) {
     return [
+
+    	    'username' => $this->faker->word,
+    	    'nome' => $this->faker->word,
+    	    'password'=> $this->faker->word,
+    	    'data_nascimento'=> $this->faker->word,
+
+
         //
     ];
 });
