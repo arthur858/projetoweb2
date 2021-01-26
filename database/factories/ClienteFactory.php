a1d8950100821dfab3618f0a4ad4f6971524725c
arthur858 <arthursylva25@gmail.com>
2021-01-25 21:26:32 -0300

    alterando o factory da tabela cliente

--- database/factories/ClienteFactory.php
+++ database/factories/ClienteFactory.php
@@ -6,7 +6,14 @@ use App\cliente;
 use Faker\Generator as Faker;
 
 $factory->define(cliente::class, function (Faker $faker) {
-    return [
+    return 
+    'nome' => $this->faker->word,
+    'password' => $this->faker->word,
+    'produto-id' => $this->faker->word,
+    'funcionario_id'=> $this->faker->word,
+    'endereco' => $this->faker->word,
+    'CPF' => $this->faker->word,
+
         //
     ];
 });
